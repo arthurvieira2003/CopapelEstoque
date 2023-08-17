b38cd2e2bb14b2b482cb971db9e69bc62c2139e4
RafaelRFAndrade <110788109+RafaelRFAndrade@users.noreply.github.com>
2023-08-17 14:20:14 -0300

    Login

--- conta_estoque/lib/pages/login_page.dart
+++ conta_estoque/lib/pages/login_page.dart
@@ -4,75 +4,154 @@ class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-      padding: const EdgeInsets.only(
-        top: 60,
-        left: 40,
-        right: 40,
-      ),
-      color: Colors.green,
-      child: ListView(
-        children: <Widget>[
-          SizedBox(
-            width: 128,
-            height: 128,
-            child: Image.asset("images/copapel.jpeg"),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            keyboardType: TextInputType.name,
-            decoration: const InputDecoration(
-              labelText: "Nome",
-              labelStyle: TextStyle(
-                color: Colors.black38,
-                fontWeight: FontWeight.w400,
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: const Color.fromARGB(255, 211, 211, 211),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: Image.asset("images/copapel.jpeg"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.name,
+              decoration: const InputDecoration(
+                labelText: "Nome",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: const TextStyle(
                 fontSize: 20,
               ),
             ),
-            style: const TextStyle(
-              fontSize: 20
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
               ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextFormField(
-            keyboardType: TextInputType.text,
-            obscureText: true,
-            decoration: const InputDecoration(
-              labelText: "Senha",
-              labelStyle: TextStyle(
-                color: Colors.black38,
-                fontWeight: FontWeight.w400,
-                fontSize: 20,
+              style: const TextStyle(fontSize: 20),
+            ),
+            Container(
+              height: 40,
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ResetPasswordPage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  "Recuperar Senha",
+                  textAlign: TextAlign.right,
+                ),
               ),
             ),
-            style: const TextStyle(fontSize: 20),
-          ),
-          Container(
-            height: 40,
-            alignment: Alignment.centerRight,
-            child: FlatButton(
-              child: Text(
-                "Recuperar Senha",
-                textAlign: TextAlign.right,
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3,1],
+                  colors: [
+                    Color.fromARGB(255, 7, 223, 61),
+                    Color.fromARGB(255, 0, 104, 47),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
               ),
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ResetPasswordPage(),
+              child: SizedBox.expand(
+                child: TextButton(
+                  onPressed: () {
+                    // aqui vai pra outra página
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(
+                        height: 28,
+                        width: 28,
+                        child: Image.asset("images/copapel.jpeg"),
+                      ),
+                    ],
                   ),
-                );
-              },
+                ),
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 40,
+              child: TextButton(
+                onPressed: () {
+                  // aqui vai pra outra página de cadastro
+                },
+                child: const Text(
+                  "Cadastrar",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class ResetPasswordPage extends StatelessWidget {
+  const ResetPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Build your ResetPasswordPage UI here
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Reset Password"),
+      ),
+      body: const Center(
+        child: Text("Reset Password Page"),
       ),
     );
   }
